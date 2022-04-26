c137ee9bbfea10568b4b161a3f3491947c383182
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2022-04-26 20:01:06 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,61 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 
-class App extends Component {
-  state = { good: 0, neutral: 0, bad: 0 };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleClick = e => {
-    const { name } = e.currentTarget;
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    });
+  const handleClick = event => {
+    const { name } = event.target;
+
+    switch (name) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  countTotalFeedbacks = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedbacks = () => {
     return good + neutral + bad;
   };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
-
+  const countPositiveFeedbackPercentage = () => {
     const allFeedbacks = good + neutral + bad;
     if (good === 0) {
       return 0;
     }
     return ((good / allFeedbacks) * 100).toFixed(1);
   };
-
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.handleClick}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={{ good, neutral, bad }}
+          onLeaveFeedback={handleClick}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedbacks() === 0 ? (
+          <Notification message="There is no feedback yet" />
+        ) : (
+          <Statistics
+            feedbacks={{ good, neutral, bad }}
+            total={countTotalFeedbacks}
+            positivePercentage={countPositiveFeedbackPercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedbacks() === 0 ? (
-            <Notification message="There is no feedback yet" />
-          ) : (
-            <Statistics
-              feedbacks={this.state}
-              total={this.countTotalFeedbacks}
-              positivePercentage={this.countPositiveFeedbackPercentage}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App2';
+import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
